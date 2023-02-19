900776958278b6a6f06fdfc3552274faadef5fdd
SandroKoraca <sandro.koraca@email.com>
2023-02-19 19:19:55 +0100

    final version

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,81 +5,57 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     string public name;
     string public symbol;
-    uint8 public decimals;
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
 
     address public owner;
-
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
-    /*
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    */
     event Mint(address indexed account, uint256 amount);
     event Burn(address indexed account, uint256 amount);
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
-        decimals = _decimals;
         totalSupply = _totalSupply;
         balanceOf[msg.sender] = totalSupply;
         owner = msg.sender;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        require(balanceOf[msg.sender] >= _value, "Nedovoljno sredstava na racunu");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-        /*
         emit Transfer(msg.sender, _to, _value);
-        */
         return true;
         
     }
 
-    /*
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-    */
-
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= balanceOf[_from], "Insufficient balance");
-        require(_value <= allowance[_from][msg.sender], "Insufficient allowance");
+        require(_value <= balanceOf[_from], "Nedovoljno sredstava na racunu");
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
-        /*
         emit Transfer(_from, _to, _value);
-        */
         return true;
     }
 
-    function ballanceOf(address _owner) public view returns (uint256){
-        return balanceOf[_owner];
+    function ballanceOf(address _account) public view returns (uint256){
+        return balanceOf[_account];
     }
 
-    function mint(address _account, uint256 _amount) public {
-        require(msg.sender == owner, "Only the owner can mint tokens");
-        totalSupply += _amount;
-        balanceOf[_account] += _amount;
-        /*
-        emit Mint(_account, _amount);
-        */
+    function mint(address _account, int _amount) public {
+        require(msg.sender == owner, "Samo vlasnik smije dodavati tokene");
+        totalSupply += uint256(_amount);
+        balanceOf[_account] += uint256(_amount);
+        emit Mint(_account, uint256(_amount));
     }
 
-    function burn(uint256 _amount) public {
-        require(msg.sender == owner, "Only the owner can burn tokens");
-        require(_amount <= balanceOf[msg.sender], "Insufficient balance");
+    function burn(address _account, uint256 _amount) public {
+        require(msg.sender == owner, "Samo vlasnik smije oduzimati tokene");
+        require(_amount <= balanceOf[_account], "Nedovoljno sredstava na racunu");
         totalSupply -= _amount;
-        balanceOf[msg.sender] -= _amount;
-        /*
-        emit Burn(msg.sender, _amount);
-        */
+        balanceOf[_account] -= _amount;
+        emit Burn(_account, _amount);
     }
 }
--- deploy.py
+++ deploy.py
@@ -40,24 +40,23 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # Connect to ganache
 w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
 chain_id = 1337
-my_addres = "0x0b5f26B6Af7E68141aF752cAdceB25847C159377"
-private_key = "0xbab0495136e3873d980cb98f001a2dee530bd8f6add74836cd2badd3967a0aed"
+my_address = "0x982eDCC34eCc6f5213fb75b03BFAfFAEEf3cF3A9"
+private_key = "0xbd988abe5f314e084434269f7b6bfeb4d0a616703692c489e9cc9819b0e21434"
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 # Get the nonce
-nonce = w3.eth.getTransactionCount(my_addres)
+nonce = w3.eth.getTransactionCount(my_address)
 
 # Our token 
 name = "VSToken"
 symbol = "VST"
-decimals = 2
-total_supply = 1000000
+total_supply = 1000
 
 # Build a transaction
-transaction = SimpleStorage.constructor(name, symbol, decimals, total_supply).build_transaction(
-    {"chainId": chain_id, "from": my_addres, "nonce": nonce}
+transaction = SimpleStorage.constructor(name, symbol, total_supply).build_transaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
 )
 
 # Sign a transaction
@@ -68,21 +67,70 @@ tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
 
+contract_address = tx_receipt['contractAddress']
+SimpleStorage2 = w3.eth.contract(address = contract_address, abi=abi)
+
 while(1):
-    print("------------------------------------- \n Izbornik:\n 1: Provjeri stanje racuna \n 2: Mint \n 3: Burn \n 4: Posalji tokene \n 5: Posalji tokene (from-to) \n 6: Izlaz")
-    unos = input("\nVaš odabir: ")
-    if(unos == 1):
-        print("1")
-    elif(unos == 2):
-        print("2")
-    elif(unos == 3):
-        print("3")
-    elif(unos == 4):
-        print("4")
-    elif(unos == 5):
-        print("5")
-    elif(unos == 6):
-        print("Izlaz! Hvala na upotrebi!")
+    print("\n------------------------------------- \nIzbornik:\n 1: Provjeri stanje racuna \n 2: Mint \n 3: Burn \n 4: Posalji tokene \n 5: Posalji tokene (from-to) \n 6: Izlaz")
+    unos = input("\nVas odabir: ")
+    if(unos == "1"): #Provjeri stanje racuna
+        unos2= input("\nOdaberite zelite li provjeriti svoju ili tudju adresu: \n 1: Svoja \n 2: Tudja \nVas odabir:")
+        if(unos2 == "1"): #Provjera svoje adrese
+            ballance = SimpleStorage2.functions.ballanceOf(my_address).call()
+            print("\nStanje vaseg racuna:", ballance)
+        elif(unos2 == "2"):
+            address = input("\nUnesite adresu: ")
+            ballance = SimpleStorage2.functions.ballanceOf(address).call()
+            print("\nStanje tudjeg racuna:", ballance)
+        else:
+            print("\nKrivi unos! Pokušajte ponovno!")
+    elif(unos == "2"): #Mint
+        unos2= input("\nOdaberite zelite li dodati tokene na svoj ili tudji racun: \n 1: Svoj \n 2: Tudji \nVas odabir:")
+        if(unos2 == "1"): #Dodavanje na svoj racun
+            numberForMint = int(input("\nKoliko tokena zelite dodati(mint): "))
+            mint = SimpleStorage2.functions.mint(my_address, numberForMint).transact({'from': my_address})
+            ballance = SimpleStorage2.functions.ballanceOf(my_address).call()
+            print("\nNovo stanje: ", ballance)
+        elif(unos2 == "2"): #Dodavanje na tudji racun
+            numberForMint = int(input("\nKoliko tokena zelite dodati(mint): "))
+            address = input("Unesite adresu: ")
+            mint = SimpleStorage2.functions.mint(address, numberForMint).transact({'from': my_address})
+            ballance = SimpleStorage2.functions.ballanceOf(address).call()
+            print("\nNovo stanje: ", ballance)
+        else:
+            print("\nKrivi unos! Pokušajte ponovno!")
+    elif(unos == "3"): #Burn
+        unos2= input("\nOdaberite zelite li smanjiti tokene na svojem ili tudjem racunu: \n 1: Svoj \n 2: Tudji \nVas odabir:")
+        if(unos2 == "1"): #Smanjivanje na svoj racun
+            numberForBurn = int(input("\nKoliko tokena zelite smanjiti(burn): "))
+            burn = SimpleStorage2.functions.burn(my_address, numberForBurn).transact({'from': my_address})
+            ballance = SimpleStorage2.functions.ballanceOf(my_address).call()
+            print("\nNovo stanje: ", ballance)
+        elif(unos2 == "2"): #Smanjivanje na tudji racun
+            numberForBurn = int(input("\nKoliko tokena zelite smanjiti(burn): "))
+            address = input("Unesite adresu: ")
+            burn = SimpleStorage2.functions.burn(address, numberForBurn).transact({'from': my_address})
+            ballance = SimpleStorage2.functions.ballanceOf(address).call()
+            print("\nNovo stanje: ", ballance)
+        else:
+            print("\nKrivi unos! Pokušajte ponovno!")
+    elif(unos == "4"): #Posalji tokene
+        tokenForSend = int(input("\nKoliko tokena zelite poslati: "))
+        address = input("Unesite adresu: ")
+        SimpleStorage2.functions.transfer(address,tokenForSend).transact({'from': my_address})
+        ballance = SimpleStorage2.functions.ballanceOf(my_address).call()
+        print("\nVase novo stanje: ", ballance)
+    elif(unos == "5"): #Posalji tokene (from-to)
+        tokenForSend = int(input("\nKoliko tokena se salje: "))
+        addressFrom = input("Unesite adresu iz koje se salje: ")
+        addressTo = input("Unesite adresu na koju se salje: ")
+        SimpleStorage2.functions.transferFrom(addressFrom, addressTo,tokenForSend).transact({'from': my_address})
+        ballanceFrom = SimpleStorage2.functions.ballanceOf(addressFrom).call()
+        ballanceTo = SimpleStorage2.functions.ballanceOf(addressTo).call()
+        print("\nNovo stanje na adresi ",addressFrom,": ", ballanceFrom)
+        print("Novo stanje na adresi ",addressTo,": ", ballanceTo)
+    elif(unos == "6"): #Izlaz
+        print("\nIzlaz! Hvala na upotrebi!")
         exit()
-    else:
-        print("Krivi unos! Pokušajte ponovno!")
+    else: #krivi unos
+        print("\nKrivi unos! Pokušajte ponovno!")
